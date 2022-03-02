0dea4831496bd8400b8d3328b8270a16ffd64a15
fahadshoukat <fraza6970@gmail.com>
2022-03-03 00:30:52 +0500

    adding and styling note component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
+
 import Header from './components/header/Header'
+import Note from './components/note/Note'
+
 import './App.css'
 
 function App() {
   return (
     <div className='app-container'>
         <Header />
+        <Note />
 
     </div>
   )
---
+++ src/components/note/Note.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './note.css'
+
+const Note = () => {
+  return (
+    <div className='note'>
+        <textarea className='note_text'/>
+        <p>12 jan 2022</p>
+    </div>
+  )
+}
+
+export default Note
