b3511800743c54aa22b54edf65beee4e5d2e0637
fahadshoukat <fraza6970@gmail.com>
2022-03-03 00:16:01 +0500

    adding and styling header component

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './components/header/Header'
+import './App.css'
+
+function App() {
+  return (
+    <div className='app-container'>
+        <Header />
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import './header.css'
+
+const Header = () => <h1>Notes</h1>
+
+export default Header
